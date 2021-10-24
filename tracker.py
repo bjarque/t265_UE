d758d2bb60c59093d696ed70a5d90d4d27db19fd
Bjarke Aagaard <bjarque@gmail.com>
2021-10-24 17:59:37 +0200

    Update tracker.py
    Added multiple engine destinations. Just add more IPs as arguments

--- tracker.py
+++ tracker.py
@@ -7,28 +7,26 @@ pipe = rs.pipeline()
 
 cfg = rs.config()
 cfg.enable_stream(rs.stream.pose)
-try:
-    TCP_IP = sys.argv[1]
-except IndexError:
-     TCP_IP = "127.0.0.1"
 
+try:
+    IPs = sys.argv[1:]
+except IndexError:
+    IPs = ["127.0.0.1"]
+print("Realsense T265 sending to" , IPs)
 TCP_PORT = 54321
 BUFFER_SIZE = 1024
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-s.connect((TCP_IP, TCP_PORT))
 starting_height = 1 # starting height in meters
 pos_scale = 100
 rot_scale = 1
 threshold = 0.0005
-
-MESSAGE = '{"MHTrack": [{"Type": "CameraSubject"}, {"FieldOfView": "true","AspectRatio": "true","FocalLength": "true","ProjectionMode": "false"}]}'
-
-s.send(MESSAGE.encode('utf-8'))
-s.close() 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-s.connect((TCP_IP, TCP_PORT))
+  
+for IP in IPs:  
+    MESSAGE = '{"MHTrack": [{"Type": "CameraSubject"}, {"FieldOfView": "true","AspectRatio": "true","FocalLength": "true","ProjectionMode": "false"}]}'
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((IP, TCP_PORT))
+    s.send(MESSAGE.encode('utf-8'))
+    s.close() 
 
 
 pipe.start(cfg)
@@ -46,6 +44,7 @@ if pose:
 
 try:
     while True:
+
         # Wait for the next set of frames from the camera
         frames = pipe.wait_for_frames()
 
@@ -58,11 +57,13 @@ try:
          
             data.translation.y += starting_height
          #   print ("x : ", - data.translation.z, " y: ", data.translation.x , "z: " , data.translation.y, " and delta is ", delta)
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             MESSAGE = '{{"MHTrack": [{{"Type":"CameraAnimation"}},{{"Location":[{},{},{}],"Rotation":[{:.4f},{:.4f},{:.4f},{:.4f}],"Scale":[1,1,1]}}]}}'.format((pos_scale*(-data.translation.z-zeroZ)),(pos_scale*(data.translation.x-zeroX)),(pos_scale*(data.translation.y-zeroY)), rot_scale*data.rotation.z, rot_scale*-data.rotation.x, rot_scale*-data.rotation.y,rot_scale*data.rotation.w)
-            s.connect((TCP_IP, TCP_PORT))
-            s.send(MESSAGE.encode('utf-8'))
-            s.close()
+          
+            for IP in IPs:
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                s.connect((IP, TCP_PORT))
+                s.send(MESSAGE.encode('utf-8'))
+                s.close()
     
 finally:
     pipe.stop()
