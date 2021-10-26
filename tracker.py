6cc30f9e140d34ba3cd8dc9ba5122e4a8b6b6933
Bjarke Aagaard <bjarque@gmail.com>
2021-10-26 10:48:11 +0200

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -2,32 +2,68 @@ import socket
 import json
 import sys
 import pyrealsense2 as rs
+from binary_fractions import TwosComplement, Binary
+import math  
+import transformations as tf
+import numpy as np
 
+PI = 3.141592653589793
 pipe = rs.pipeline()
 
 cfg = rs.config()
 cfg.enable_stream(rs.stream.pose)
 
+def floatToBytes(number, bitsOfIntegral):
+    number = str(number)
+    # convert floating number in string
+    # to two complement with variable
+    # length of integral part. Always 
+    # 24bits, and returns 3 bytes
+    whole, dec = str(number).split(".") 
+   # if int(whole)> (1<<bitsOfIntegral)/2:
+    #    whole = str((1<<bitsOfIntegral)/2)
+  #      whole = whole[0:3]
+  #  elif int(whole)< -(1<<bitsOfIntegral)/2:
+   #     whole = "-" + str((1<<bitsOfIntegral)/2)
+   #     whole = whole[0:4]
+  
+    newNumber = TwosComplement(float(whole + "."+dec))
+    try:
+        idx = newNumber.index(".")
+    except ValueError:
+        newNumber ="0.0"
+    whole, dec = str(newNumber).split(".") 
+    insert ="0"
+    if number[0]=="-":
+        insert ="1"
+    if len(whole)< (bitsOfIntegral+1):
+        for i in range(0,(bitsOfIntegral+1-len(whole))):
+            whole =insert + whole
+     
+    if len(dec)>(24-bitsOfIntegral-1):
+        dec = dec[0:(24-bitsOfIntegral-1)]
+    elif len(dec)<(24-bitsOfIntegral-1):
+        for i in range(0,((24-bitsOfIntegral-1)-len(dec))):
+            dec = dec + "0"
+          
+    final = whole+dec
+    return int(final,2).to_bytes(3, byteorder='big')
+
 try:
     IPs = sys.argv[1:]
 except IndexError:
     IPs = ["127.0.0.1"]
-print("Realsense T265 sending to" , IPs)
-TCP_PORT = 54321
+
+
+
+print("Realsense T265 sending to" , IPs, " in FreeD protocol")
+TCP_PORT = 40000
 BUFFER_SIZE = 1024
 
 starting_height = 1 # starting height in meters
-pos_scale = 100
-rot_scale = 1
+pos_scale = 1000
+rot_scale =  128
 threshold = 0.0005
-  
-for IP in IPs:  
-    MESSAGE = '{"MHTrack": [{"Type": "CameraSubject"}, {"FieldOfView": "true","AspectRatio": "true","FocalLength": "true","ProjectionMode": "false"}]}'
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect((IP, TCP_PORT))
-    s.send(MESSAGE.encode('utf-8'))
-    s.close() 
-
 
 pipe.start(cfg)
 
@@ -37,36 +73,50 @@ frames = pipe.wait_for_frames()
 pose = frames.get_pose_frame()
 if pose:
     data = pose.get_pose_data()
-    zeroX = data.translation.x
-    zeroY = data.translation.y 
-    zeroZ = data.translation.z
 
+H_aeroRef_T265Ref = np.array([[0,0,-1,0],[1,0,0,0],[0,-1,0,0],[0,0,0,1]])
+H_T265body_aeroBody = np.linalg.inv(H_aeroRef_T265Ref)
 
 try:
     while True:
 
+        my_bytes = bytearray()
         # Wait for the next set of frames from the camera
         frames = pipe.wait_for_frames()
 
         # Fetch pose frame
         pose = frames.get_pose_frame()
         if pose:
-            # Print some of the pose data to the terminal
             data = pose.get_pose_data()
-      #      if data.translation.z
-         
+            q = data.rotation
             data.translation.y += starting_height
-         #   print ("x : ", - data.translation.z, " y: ", data.translation.x , "z: " , data.translation.y, " and delta is ", delta)
-            MESSAGE = '{{"MHTrack": [{{"Type":"CameraAnimation"}},{{"Location":[{},{},{}],"Rotation":[{:.4f},{:.4f},{:.4f},{:.4f}],"Scale":[1,1,1]}}]}}'.format((pos_scale*(-data.translation.z-zeroZ)),(pos_scale*(data.translation.x-zeroX)),(pos_scale*(data.translation.y-zeroY)), rot_scale*data.rotation.z, rot_scale*-data.rotation.x, rot_scale*-data.rotation.y,rot_scale*data.rotation.w)
-          
+            H_T265Ref_T265body = tf.quaternion_matrix([data.rotation.w, data.rotation.x,data.rotation.y,data.rotation.z]) # in transformations, 
+            H_aeroRef_aeroBody = H_aeroRef_T265Ref.dot( H_T265Ref_T265body.dot( H_T265body_aeroBody ))
+            rpy_rad = np.array( tf.euler_from_matrix(H_aeroRef_aeroBody, 'rxyz') )
+        
+            my_bytes.extend((bytes.fromhex('D1'))) # message type
+            my_bytes.extend((bytes.fromhex('01'))) #camera id
+            my_bytes.extend(floatToBytes(rpy_rad[2]*(180/PI),8)) # tilt
+            my_bytes.extend(floatToBytes((rpy_rad[1])*(180/PI),8)) # pan 0 pitch
+            my_bytes.extend(floatToBytes((rpy_rad[0])*(180/PI),8)) # pan 0 pitch
+            my_bytes.extend(floatToBytes(pos_scale*data.translation.x,17))
+            my_bytes.extend(floatToBytes(pos_scale*-data.translation.z,17))
+            my_bytes.extend(floatToBytes(pos_scale*data.translation.y,17))
+            my_bytes.extend(floatToBytes('120.0',17))
+            my_bytes.extend(floatToBytes("120.0",17))
+            my_bytes.extend((bytes.fromhex('F1'))) # message type
+            my_bytes.extend((bytes.fromhex('A1'))) # message type
+            chkInit = 40
+            for byte in my_bytes:
+                chkInit -= byte # The checksum is calculated by subtracting (modulo 256) each byte of themessage, including the message type, from 40 
+            my_bytes.extend((chkInit % 256).to_bytes(1, byteorder='big')) # message type
+            
             for IP in IPs:
                 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 s.connect((IP, TCP_PORT))
-                s.send(MESSAGE.encode('utf-8'))
+                s.send(my_bytes)
                 s.close()
     
 finally:
     pipe.stop()
 
-
-    
